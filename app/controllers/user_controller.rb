ccec7e3a9a8130babe90f2b89d9112d55ac1c0ea
Andrew <aeason96@vt.edu>
2018-08-01 10:10:29 -0400

    added simple user creation page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,7 +6,7 @@ class UserController < ApplicationController
   end
 
   def show
-    @article = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def new
@@ -15,11 +15,12 @@ class UserController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    redirect_to request.referrer if @user.save
   end
 
   private
 
   def user_params
-    params.require(:user).permit(user)
+    params.permit(:username, :password)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  validates :username, presence: true
+  validates :username, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 5 }
 end
